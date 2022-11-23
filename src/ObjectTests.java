d76b0b5d19adeecabf2aa6839544bd653ba3e10b
ElenaKravtsova <elena@localhost>
2022-11-23 18:56:26 +0200

    Homework7

--- src/ObjectTests.java
+++ src/ObjectTests.java
@@ -41,12 +41,59 @@ class ObjectTests {
 		assertFalse(Strings.isAnagram(word, "yellob"));
 		assertFalse(Strings.isAnagram(word, "yello"));
 		assertFalse(Strings.isAnagram(word, "yelllo"));
+		assertTrue(Strings.isAnagram("yttttoooo", "ooootttty"));
 	}
 
 	@Test
+	@Disabled
 	void sortStringNumbersTest() {
 		String[] array1 = { "-1", "1", "2", "118", "-11", "2", "0", "127", "-128" };
 		Strings.sortStringNumbers(array1);
 		assertArrayEquals(new String[] { "-128", "-11", "-1", "0", "1", "2", "2", "118", "127" }, array1);			
 	}
+	
+
+	@Test
+	void javaVariableTest() {
+		assertTrue("java".matches(Strings.javaNameExp()));
+		assertFalse("1java".matches(Strings.javaNameExp()));
+		assertFalse("_".matches(Strings.javaNameExp()));
+		assertTrue("__".matches(Strings.javaNameExp()));
+		assertTrue("java_1_2".matches(Strings.javaNameExp()));
+		assertTrue("$".matches(Strings.javaNameExp()));
+		assertFalse("$ _".matches(Strings.javaNameExp()));
+
+	}
+
+	@Test
+	void ipV4OctetTest() {
+		assertTrue("0".matches(Strings.ipV4Octet()));
+		assertTrue("9".matches(Strings.ipV4Octet()));
+		assertTrue("11".matches(Strings.ipV4Octet()));
+		assertTrue("199".matches(Strings.ipV4Octet()));
+		assertTrue("255".matches(Strings.ipV4Octet()));
+		assertTrue("247".matches(Strings.ipV4Octet()));
+		assertFalse("01".matches(Strings.ipV4Octet()));
+		assertFalse("000".matches(Strings.ipV4Octet()));
+		assertFalse("00".matches(Strings.ipV4Octet()));
+		assertFalse("001".matches(Strings.ipV4Octet()));
+		assertFalse("020".matches(Strings.ipV4Octet()));
+		assertFalse("256".matches(Strings.ipV4Octet()));
+		assertFalse("0256".matches(Strings.ipV4Octet()));
+
+	}
+
+	@Test
+	void ipV4Test() {
+		assertTrue("0.0.0.0".matches(Strings.ipV4()));
+		assertTrue("100.0.0.0".matches(Strings.ipV4()));
+		assertTrue("255.255.255.255".matches(Strings.ipV4()));
+		assertFalse("01.0.0.0".matches(Strings.ipV4()));
+		assertFalse("000.0.0.0".matches(Strings.ipV4()));
+		assertFalse("00.0.0.0".matches(Strings.ipV4()));
+		assertFalse("001000.0".matches(Strings.ipV4()));
+		assertFalse("020.0.0.0.".matches(Strings.ipV4()));
+		assertFalse("256.0.0.0".matches(Strings.ipV4()));
+	}
+
 }
--- src/Strings.java
+++ src/Strings.java
@@ -5,56 +5,97 @@ public class Strings {
 	/**
 	 * @param str1
 	 * @param str2
-	 * @return true if string2 is anagram of string1:
-	 * - the same length 2;
-	 * - the same symbols just in different order2
-	 * complexity O[N]
-	 * two passes
+	 * @return true if string2 is anagram of string1: - the same length 2; - the
+	 *         same symbols just in different order2 complexity O[N] two passes
 	 */
 	public static boolean isAnagram(String str1, String str2) {
-		int counter = 0;
-		int str1Length = str1.length();
-		if (str1 != null && str2 != null && str1Length == str2.length()) {
-			int[] arrayOfMarks = new int[Character.MAX_VALUE + 1];
-			int symbol1, symbol2;
-			for (int i = 0; i < str1Length; i++) {
-				symbol1 = str1.charAt(i);
-				arrayOfMarks[symbol1]++;
-				symbol2 = str2.charAt(i);
-				arrayOfMarks[symbol2]--;
-				if (symbol1 == symbol2 || (symbol1 != symbol2 && arrayOfMarks[symbol2] == 0)) {
-					counter++;
-				}
-				if (symbol1 != symbol2 && arrayOfMarks[symbol1] == 0) {
-					counter++;
+		boolean res = false;
+		if (str1.length() == str2.length()) {
+			int[] helper = new int[Byte.MAX_VALUE]; // 127, the condition of the problem is only numbers and letters
+			fillHelper(helper, str1);
+			res = true;
+			int index = 0;
+			int length = str1.length();
+			while (index < length && res) {
+				int indexSymbol = str2.charAt(index); // this is not an array, but a method, so it's better to take it
+														// out separately
+				if (helper[indexSymbol] == 0) {
+					res = false;
+				} else {
+					helper[indexSymbol]--;
+					index++;
 				}
 			}
 		}
-		return counter == str1Length;
+		return res;
+	}
+
+	private static void fillHelper(int[] helper, String str1) {
+		int length = str1.length();
+		for (int i = 0; i < length; i++) {
+			int index = str1.charAt(i);
+			helper[index]++;
+		}
 	}
 
 	/**
-	 * @param str contains decimal presentation of a number from Byte.MIN_VALUE to Byte.MAX_VALUE 
-	 * (verification in the method is not required).
-	 * @return method sorts input array on the place (it returns nothing).
-	 * Has complexity O[N]
+	 * @param str contains decimal presentation of a number from Byte.MIN_VALUE to
+	 *            Byte.MAX_VALUE (verification in the method is not required).
+	 * @return method sorts input array on the place (it returns nothing). Has
+	 *         complexity O[N]
 	 */
 	public static void sortStringNumbers(String[] array) {
-		int[] arrayOfMarks = new int[(Byte.MAX_VALUE+1)*2];
-		int number;
-		int k = 0;
-		for (int i = 0; i < array.length; i++) {
-			number = Integer.parseInt(array[i]);
-			arrayOfMarks[number + 128] += 1;
-		}
-		for (int j = 0; j < arrayOfMarks.length; j++) {
-			if (arrayOfMarks[j] != 0) {
-				do {
-					array[k] = String.valueOf(j - 128);
-					arrayOfMarks[j]--;
-					k++;
-				} while (arrayOfMarks[j] != 0);
+		int offset = Byte.MAX_VALUE + 1;// how much to move
+		int[] helper = new int[offset * 2];
+		fillHelper(helper, array, offset);
+		sortFromHelper(array, helper, offset);
+	}
+
+	private static void sortFromHelper(String[] array, int[] helper, int offset) {
+		int arrayIndex = 0;
+		for (int i = 0; i < helper.length; i++) {
+			for (int j = 0; j < helper[i]; j++) {// it can only be entered if i!=0
+				array[arrayIndex] = getStringNumberFromIndex(i, offset);
 			}
 		}
+	}
+
+	private static String getStringNumberFromIndex(int index, int offset) {
+		int number = index - offset;
+		return Integer.toString(number);
+	}
+
+	private static void fillHelper(int[] helper, String[] array, int offset) {
+		for (int i = 0; i < array.length; i++) {
+			int index = getIndexFromStringNumber(array[i], offset);
+			helper[index]++;
+		}
+	}
+
+	private static int getIndexFromStringNumber(String string, int offset) {
+		int number = Integer.parseInt(string);
+		return number + offset;
+	}
+
+	/**
+	 * may begin with a small or large letter, or a $ sign may begin with underscore
+	 * (but it cannot be the only character) after this can be any characters in any
+	 * number
+	 * @return
+	 */
+	public static String javaNameExp() {
+		return "([a-zA-Z$][\\w$]*)|(_[\\w$]+)";
+	}
+
+	public static String ipV4Octet() {
+		return "([0-9]|[1-9][0-9]|[1][0-9]{2}|[2][0-4][0-9]|[2][5][0-5])";
+	}
+
+	public static String ipV4() {
+		String ipV4 = "";
+		for (int i = 0; i < 3; i++) {
+			ipV4 += ipV4Octet() + "\\.";
+		}
+		return ipV4 + ipV4Octet();
 	}
 }
